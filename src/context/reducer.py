2436b52d0e5c1ba1623d8205150a5b93d8212f81
Retr0327 <philcoke35@gmail.com>
2022-08-30 23:40:53 +0800

    Update reducer

--- src/context/reducer.py
+++ src/context/reducer.py
@@ -1,12 +1,13 @@
 from typing import Any, Union
+from .actions import WSGKind, Action
 
 
-def reducer(state, action) -> Union[str, Any]:
+def reducer(state, action: Action) -> Union[str, Any]:
     """The reducer function generates new states."""
 
     initial_state = (state != None) if state else ""
 
-    if action["type"] == "ADD_WSG":
-        return action["payload"]
+    if action.kind == WSGKind.ADD_WSG:
+        return action.payload
 
     return initial_state
