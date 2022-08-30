23c75d790f0ebeea3d651bee26087f0eabdff23e
Retr0327 <philcoke35@gmail.com>
2022-08-31 01:43:14 +0800

    Run with asyncio

--- src/controllers/ckip/wsg.py
+++ src/controllers/ckip/wsg.py
@@ -1,3 +1,4 @@
+import asyncio
 from typing import List
 from context import use_WSG, WSGKind
 from utils.ckip.wsg import WordSegmentation
@@ -17,4 +18,4 @@ def handle_create_wsg(nlp_model: str, sentence_list: List[str]) -> List[str]:
     ws_result = WordSegmentation(nlp_model, sentence_list).segment()
     dispatch = use_WSG()[1]
     dispatch({"kind": WSGKind.ADD_WSG, "payload": ws_result})
-    return add_multiple_textsubscripts("ws", ws_result)
+    return asyncio.run(add_multiple_textsubscripts("ws", ws_result))
